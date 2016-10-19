74055c2a223c7d0555a213bf003a5532379b1eb5
Benjamin <Benjamin.z.jin@gmail.com>
2016-10-18 19:41:13 -0700

    started tests

--- ExtDomainModelTests/DescriptionTests.swift
+++ ExtDomainModelTests/DescriptionTests.swift
@@ -2,34 +2,34 @@
 //  DescriptionTests.swift
 //  ext-domain-model
 //
-//  Created by iGuest on 10/18/16.
-//  Copyright © 2016 iGuest. All rights reserved.
+//  Created by Benjamin Jin on 10/18/16.
+//  Copyright © 2016 Benjamin Jin. All rights reserved.
 //
 
 import XCTest
 
 class DescriptionTests: XCTestCase {
-
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testMoneyDescription() {
+        let tenUSD = Money(amount: 10, currency: "USD")
+        let fiveGBP = Money(amount: 5, currency: "GBP")
+        let fifteenEUR = Money(amount: 15, currency: "EUR")
+        let fifteenCAN = Money(amount: 15, currency: "CAN")
+        
+        XCTAssert(tenUSD.description == "USD10.0")
+        XCTAssert(fiveGBP.description == "GBP5.0")
+        XCTAssert(fifteenEUR.description == "EUR15.0")
+        XCTAssert(fifteenCAN.description == "CAN15.0")
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testJobDescription() {
+        
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testPersonDescription() {
+        
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testFamilyDescription() {
+        
     }
-
 }
