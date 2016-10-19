e468d1be4133d9020cfc00ee3cd9c7f6aaf76e5e
Benjamin <Benjamin.z.jin@gmail.com>
2016-10-18 17:41:11 -0700

    Initial Commit

---
+++ ext-domain-model/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ext-domain-model
+//
+//  Created by iGuest on 10/18/16.
+//  Copyright © 2016 iGuest. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
